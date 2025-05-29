e38565f7b79189623900dd7312518d29a8d441dc
Kevil Shaji <kevilshaji@student.sfit.ac.in>
2025-05-29 14:42:31 +0530

    chp 10

---
+++ chp10/src/q1/Main.java
@@ -0,0 +1,27 @@
+package q1;
+
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String str1 = "     Kevil       ";
+		String str2 = "IS a GOD";
+		System.out.println(str1.length());
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.concat(str2));
+		System.out.println(str1.trim());
+		System.out.println(str1.replace("K","D").concat(str2));
+		int i = 10;
+		Integer s = Integer.valueOf(i);
+		System.out.println(s);
+		
+		
+		
+		int b = 10;
+		Integer w = b;
+		System.out.println("warppper classss"+w);
+		
+	}
+}
